b1eb060f5605ee2b3cbbb3b388a210f98b80e1ad
Marko Radulovic <markoradulovic012@gmail.com>
2023-02-13 21:05:45 +0100

    build fix home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
